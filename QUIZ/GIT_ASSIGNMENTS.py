99a8130d5eb30673765d94a0258b044aad0700e6
nasrax <65534442+nasrax@users.noreply.github.com>
2020-08-28 15:58:57 +0300

    Update GIT_ASSIGNMENTS.py

--- QUIZ/GIT_ASSIGNMENTS.py
+++ QUIZ/GIT_ASSIGNMENTS.py
@@ -5,7 +5,7 @@ A.Emacs
 B.Vim
 C.Notepad++
 D.Bash
-
+Answer - B
 QUESTION 2
 Before you install Git, which of the following prerequisite products must be present and configured on your local OS?
 
@@ -13,7 +13,7 @@ A.Jakarta Enterprise Edition web profile compliant server
 B.Java Development Kit 1.8 or newer
 C.Apache Maven
 D.Nothing
-
+Answer - D
 QUESTION 3
 After you install Git and prior to issuing the first commit, which two configuration properties does the tool expect to be configured?
 
@@ -21,7 +21,7 @@ A.username and email address
 B.username and password
 C.email address and password
 D.username and IP address
-
+Answer - A
 QUESTION 4
 Which of the following is not a Git configuration scope?
 
@@ -29,7 +29,7 @@ A.Local
 B.User
 C.System
 D.Global
-
+Answer - B
 QUESTION 5
 Who is attributed with inventing Git?
 
@@ -37,7 +37,7 @@ A.Junio C. Hamano
 B.James Gosling
 C.Kohsuke Kawaguchi
 D.Linus Torvalds
-
+Answer - D
 QUESTION 6
 After you initialize a new Git repository and create a file named git-quiz.html, which of the following commands will not work if issued?
 
@@ -45,7 +45,7 @@ A.git add git-quiz.html
 B.git status
 C.git add .
 D.git commit -m "git quiz file added"
-
+Answer - D
 QUESTION 7
 Under which circumstance should you use a single dash within a Git command, as opposed to a double dash?
 
@@ -53,7 +53,7 @@ A.When using a single-character option
 B.When using a multicharacter option
 C.When issuing Git commands with multiple options
 D.When referencing a Git alias
-
+Answer - A
 QUESTION 8
 Which vendor acquired GitHub for $7.5 billion in June 2018?
 
@@ -61,7 +61,7 @@ A.Oracle
 B.Microsoft
 C.IBM
 D.Google
-
+Answer - B
 QUESTION 9
 Which command should you use to initialize a new Git repository?
 
@@ -69,7 +69,7 @@ A.git bash
 B.git install
 C.git init
 D.git start
-
+Answer - C
 QUESTION 10
 Which file can you configure to ensure that certain file types are never committed to the local Git repository?
 
@@ -77,5 +77,5 @@ A.ignore.git
 B..gitignore
 C.gitignore.txt
 D.git.ignore
-
+Answer - B
 
