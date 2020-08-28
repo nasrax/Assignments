f0285ea8cebd89cb8d3455c6d3e32b94c4314d44
sammy <sammy@gmail.com>
2020-08-28 13:55:40 +0300

    FIRST COMMIT QUIZ ASSIGNMENTS

---
+++ QUIZ/GIT_ASSIGNMENTS.py
@@ -0,0 +1,81 @@
+QUESTION 1
+What is the default text editor for the Bash shell with a Windows-based Git install?
+
+A.Emacs
+B.Vim
+C.Notepad++
+D.Bash
+
+QUESTION 2
+Before you install Git, which of the following prerequisite products must be present and configured on your local OS?
+
+A.Jakarta Enterprise Edition web profile compliant server
+B.Java Development Kit 1.8 or newer
+C.Apache Maven
+D.Nothing
+
+QUESTION 3
+After you install Git and prior to issuing the first commit, which two configuration properties does the tool expect to be configured?
+
+A.username and email address
+B.username and password
+C.email address and password
+D.username and IP address
+
+QUESTION 4
+Which of the following is not a Git configuration scope?
+
+A.Local
+B.User
+C.System
+D.Global
+
+QUESTION 5
+Who is attributed with inventing Git?
+
+A.Junio C. Hamano
+B.James Gosling
+C.Kohsuke Kawaguchi
+D.Linus Torvalds
+
+QUESTION 6
+After you initialize a new Git repository and create a file named git-quiz.html, which of the following commands will not work if issued?
+
+A.git add git-quiz.html
+B.git status
+C.git add .
+D.git commit -m "git quiz file added"
+
+QUESTION 7
+Under which circumstance should you use a single dash within a Git command, as opposed to a double dash?
+
+A.When using a single-character option
+B.When using a multicharacter option
+C.When issuing Git commands with multiple options
+D.When referencing a Git alias
+
+QUESTION 8
+Which vendor acquired GitHub for $7.5 billion in June 2018?
+
+A.Oracle
+B.Microsoft
+C.IBM
+D.Google
+
+QUESTION 9
+Which command should you use to initialize a new Git repository?
+
+A.git bash
+B.git install
+C.git init
+D.git start
+
+QUESTION 10
+Which file can you configure to ensure that certain file types are never committed to the local Git repository?
+
+A.ignore.git
+B..gitignore
+C.gitignore.txt
+D.git.ignore
+
+
